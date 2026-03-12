31f6c3d00514945436e21d00ca43a58846a6481c
pyqpyqpyqpyq789 <69802594+pyqpyqpyqpyq789@users.noreply.github.com>
2026-03-12 17:25:48 +0800

    Update Voting2.py

--- Voting2.py
+++ Voting2.py
@@ -688,18 +688,18 @@ def Explain(model_results, sample_id, model_metr, target, names, project_path):
     # shap.save_html(project_path + 'plots/force_plot.html',
     #                shap.force_plot(Expected, merged_shap, X.iloc[X_id]))
 
-    # # 绘制shap决策图
-    # error_indices = np.where(y_pred_list != y_list)[0]
-    # print('error_indices', error_indices)
-    # # # 多样本可视化探索
-    # shap.decision_plot(Expected, merged_shap, X.iloc[X_id],
-    #                    highlight=error_indices,  # 突出显示错误样本（核心参数）
-    #                    show=False,
-    #                    ignore_warnings=True)
-    # plt.title('SHAP decision plot')
-    # plt.tight_layout()
-    # plt.savefig(project_path + 'plots/decision.jpg', dpi=300, bbox_inches='tight')
-    # plt.close()
+    # 绘制shap决策图
+    error_indices = np.where(y_pred_list != y_list)[0]
+    print('error_indices', error_indices)
+    # # 多样本可视化探索
+    shap.decision_plot(Expected, merged_shap, X.iloc[X_id],
+                       highlight=error_indices,  # 突出显示错误样本（核心参数）
+                       show=False,
+                       ignore_warnings=True)
+    plt.title('SHAP decision plot')
+    plt.tight_layout()
+    plt.savefig(project_path + 'plots/decision.jpg', dpi=300, bbox_inches='tight')
+    plt.close()
     # Deci_plot(y_pred=y_pred_list, y_true=y_true_list, sample_index=3)  # 绘制shap决策图
 
     # 指定样本绘制瀑布图
@@ -800,6 +800,7 @@ if __name__ == "__main__":
     if not os.path.exists(project_path+'plots'):
         os.makedirs(project_path+'plots')
     main()
+
 
 
 
